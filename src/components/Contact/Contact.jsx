bcc0940643a23b9e44a35fa41fa4a67745fb5eba
Fellipe Castro <fellipecastro27d@gmail.com>
2025-03-06 21:19:48 -0300

    Projeto atualizado

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,22 +1,36 @@
 import { IoCopy } from "react-icons/io5";
 import styles from "./Contact.module.css";
+import { useState } from "react";
 
 function Contact() {
+    const [emailCopied, setEmailCopied] = useState(false); 
+    const email = "fehcastru@gmail.com";
+    const copyEmailToClipboard = () => {
+        navigator.clipboard.writeText(email)
+            .then(() => {
+                setEmailCopied(true);
+                setTimeout(() => setEmailCopied(false), 2500);
+            })
+            .catch((err) => console.error("Erro ao copiar e-mail: ", err));
+    };
+
     return (
         <section className={styles.contact} id="contact">
-            <div class={styles.container}>
-                <div class={styles.textContainer}>
+            <div className={styles.container}>
+                <div className={styles.textContainer}>
                     <h2>Entre em contato comigo!</h2>
                     <p>
                         Me mande um e-mail para trocarmos
                         ideias ou darmos início a um novo projeto!
                     </p>
                 </div>
-                <div class={styles.emailContainer}>
-                    <button>
+                <div className={styles.emailContainer}>
+                    <button onClick={copyEmailToClipboard}>
                         <IoCopy />
                     </button>
-                    <span>fehcastru@gmail.com</span>
+                    <span>
+                        {emailCopied ? "E-mail copiado!" : email}
+                    </span>
                 </div>
             </div>
         </section>
